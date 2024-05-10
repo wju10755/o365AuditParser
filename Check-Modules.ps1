ccf2e53adaebf0a387a47622fe190b9333f25689
MITS <73495665+wju10755@users.noreply.github.com>
2024-05-09 20:27:18 -0400

    Update Check-Modules.ps1
    Removed AzureAD module check

--- Check-Modules.ps1
+++ Check-Modules.ps1
@@ -6,14 +6,3 @@ if (-Not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
 } else {
     Write-Output "NuGet is already installed."
 }
-
-# AzureAD Module Check
-If (Get-Module -ListAvailable -name AzureAD) {
-    Write-Output "AzureAD PowerShell Module Detected"
-    Import-Module AzureAD
-} else {
-    Write-Host -ForegroundColor Red "AzureAD Module not found!"
-    Write-Output "Installing AzureAD Module, please wait..."
-    Install-Module -Name AzureAD -Scope CurrentUser -AllowClobber -Force
-}
- 
