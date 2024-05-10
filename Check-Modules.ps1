1f9a58b9e7d93f05d1d23512773e171e051c6977
MITS <73495665+wju10755@users.noreply.github.com>
2024-05-09 21:37:09 -0400

    Update Check-Modules.ps1

--- Check-Modules.ps1
+++ Check-Modules.ps1
@@ -1,8 +1,8 @@
 # NuGet Provider Check
 if (-Not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
-    Write-Output "NuGet is not installed. Installing NuGet..."
+    Write-Output "NuGet provider is not installed. Installing NuGet..."
     Install-PackageProvider -Name NuGet -Force -Confirm:$false -ForceBootstrap
     Import-PackageProvider -Name NuGet
 } else {
-    Write-Output "NuGet is already installed."
+    Write-Output "NuGet provider is already installed."
 }
