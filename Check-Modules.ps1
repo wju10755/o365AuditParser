981b1a504f85085932fb07c7b23746847c995a39
MITS <Billu@advancestuff.com>
2024-05-29 10:56:01 -0400

    Added automation to Nuget package provider installation

--- Check-Modules.ps1
+++ Check-Modules.ps1
@@ -1,19 +1,30 @@
-# NuGet Provider Check
-if (-Not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
-    Write-Output "NuGet is not installed. Installing NuGet..."
-    Install-PackageProvider -Name NuGet -Force -Confirm:$false -ForceBootstrap
-    Import-PackageProvider -Name NuGet
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait('~');
+if ((Get-Packageprovider -Name NuGet) -eq $null) {
+    #Write-Output 'NuGet package provider not found!'
+    #Write-Output 'Installing NuGet 2.8.5.201 Provider, Please Wait..'
+    try {
+        Install-Packageprovider -name nuget -requiredVersion 2.8.5.201 -force -ErrorAction Stop | Out-Null
+        #Write-Output 'NuGet package provider installed successfully.'
+    } catch {
+        #Write-Output "Failed to install NuGet package provider: $_"
+    }
 } else {
-    Write-Output "NuGet is already installed."
+    #Write-Output 'NuGet package provider is installed!'
 }
 
-# AzureAD Module Check
-If (Get-Module -ListAvailable -name AzureAD) {
-    Write-Output "AzureAD PowerShell Module Detected"
-    Import-Module AzureAD
-} else {
-    Write-Host -ForegroundColor Red "AzureAD Module not found!"
-    Write-Output "Installing AzureAD Module, please wait..."
-    Install-Module -Name AzureAD -Scope CurrentUser -AllowClobber -Force
-}
+#Write-Output ' Done.'
+
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait('~');
+#Write-Host
+#Write-Host
+#Write-Host 'Validating Availability of PSWindows Update Module, Please Wait...' 
+if ((Get-Module -Name PSWindowsUpdate) -eq $null) {
+    #Write-Host -ForegroundColor Yellow 'PSWindowsUpdate module not found!'
+    #Write-Host 'Installing PSWindowsUpdate Module, Please Wait...'
+    Install-Module -name PSWindowsUpdate -force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null
+    }
+
  
